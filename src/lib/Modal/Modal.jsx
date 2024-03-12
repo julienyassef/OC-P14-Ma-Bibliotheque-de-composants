2fe57c77125c69cea909291c763fcb4ecb98056d
julien yassef <juyassef@gmail.com>
2024-03-12 11:16:22 +0100

    ajout cross default pour close

--- src/lib/Modal/Modal.jsx
+++ src/lib/Modal/Modal.jsx
@@ -4,7 +4,15 @@ import './ModalEmployeeCreated.scss'
 //React
 import { useEffect } from 'react';
 
-function Modal({ isOpen, handleClose, title, content, className, disableScroll = true }) {
+const defaultCloseIcon = (
+  <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+    <line x1="18" y1="6" x2="6" y2="18"></line>
+    <line x1="6" y1="6" x2="18" y2="18"></line>
+  </svg>
+);
+
+function Modal({ isOpen, handleClose, title, content, className, disableScroll = true, closeIcon = defaultCloseIcon }) {
+  
   
   
   useEffect(() => {
@@ -20,7 +28,9 @@ function Modal({ isOpen, handleClose, title, content, className, disableScroll =
   return isOpen ? (
     <div className={`modal ${className}`}>
       <div className="modal__content">
-        <span className="modal__content__close" onClick={handleClose}>X</span>
+        <span className="modal__content__close" onClick={handleClose}>
+        {closeIcon}
+        </span>
         {title && <h2>{title}</h2>}
         <div>{content}</div>
       </div>
