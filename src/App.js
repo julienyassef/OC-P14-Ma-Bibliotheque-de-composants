2b06d59a8f0a4d8e648dae3140c17cadfbf69edf
julien yassef <juyassef@gmail.com>
2024-03-13 19:25:53 +0100

    ajout closeIcon et closeClass en props

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ function App() {
   const handleOpenModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
+  // Icon Cross close par défault sur la modal
+  const defaultCloseIcon = (
+    <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+      <line x1="18" y1="6" x2="6" y2="18"></line>
+      <line x1="6" y1="6" x2="18" y2="18"></line>
+    </svg>
+  );
+  
+
   return (
     <div className="App">
       <button onClick={handleOpenModal}>Create Employee</button>
@@ -25,6 +34,8 @@ function App() {
         className="custom-modal-style"
         disableScroll={false}
         centeredModal={true}
+        closeIcon={defaultCloseIcon}
+        closeClass={`custumClose`}
       />
     </div>
   );
--- src/lib/Modal/Modal.jsx
+++ src/lib/Modal/Modal.jsx
@@ -2,16 +2,7 @@
 import './Modal.scss'
 
 //React
-import React, { useEffect, useRef } from 'react';
-
-
-// Icon Cross close par défault sur la modal
-const defaultCloseIcon = (
-  <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-    <line x1="18" y1="6" x2="6" y2="18"></line>
-    <line x1="6" y1="6" x2="18" y2="18"></line>
-  </svg>
-);
+import React, { useEffect, useRef, useState } from 'react';
 
 function Modal({ 
   isOpen, 
@@ -23,12 +14,13 @@ function Modal({
   closeOnClickOutside = true,
   enableCloseIconClick = true,
   disableScroll = true, 
-  closeIcon = defaultCloseIcon,
-  centeredModal = true 
+  closeIcon,
+  closeClass = '',
+  centeredModal = true, 
 }) {
-  
-  const modalRef = useRef(null);
 
+  //ref pour accéder au DOm de la modal
+  const modalRef = useRef(null);
 
   // logique pour empecher de scroller lorsque la modal est ouverte
   useEffect(() => {
@@ -76,25 +68,33 @@ function Modal({
     }
   };
   
-  return isOpen ? (
-    <div className={`modal ${centeredModal ? 'centeredModal' : ''}`} ref={modalRef}>
-      <span className="modal__iconClose" onClick={handleIconClick}>
-      {closeIcon}
-      </span>
-      {title && <h2 className="modal__title">
-        {title}
-      </h2>}
-      <div className="modal__content" >
-        {content}
-        {showCloseLink && (
-        <a href="#" onClick={handleClose} className="modal__linkClose">
-          Fermer la modal
-        </a>
-      )}
-      </div>
-    </div>
-    
-  ) : null;
-}
 
-export default Modal
+  return (
+    <>
+      {<div className="modal-overlay"></div>}
+      {isOpen ? (
+        <div 
+          className={`modal ${centeredModal ? 'centeredModal' : ''}`}
+          ref={modalRef}
+        >
+          <span className={`${closeClass}`}  onClick={handleIconClick}>
+            {closeIcon}
+          </span>
+          {title && <h2 className="modal__title">
+            {title}
+          </h2>}
+          <div className="modal__content">
+            {content}
+            {showCloseLink && (
+              <a href="#" onClick={handleClose} className="modal__linkClose">
+                Fermer la modal
+              </a>
+            )}
+          </div>
+        </div>
+      ) : null}
+    </>
+  )
+} 
+  
+export default Modal;
